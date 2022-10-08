07ab9120162ce5c39fc3698a60bf2aa1aaec0fc1
Kevin Wu <superenderman1166@gmail.com>
2022-10-08 10:53:54 -0400

    changes happened

--- BillChen.py
+++
@@ -1,1 +0,0 @@
-print("hello, Bill Chen's test")
---
+++ numbers_and_math.py
@@ -0,0 +1,15 @@
+x = 'sutonn'
+# Each variables has its own data type.
+# Four basic data type: Integer, float(decimal), string(something in the quote)(single quote, double quote no difference, but must be the same)
+# boolean(2 values, either true or false)
+y = 100
+z = 90.5
+l = True
+q = False
+print(x, y, z, l, q)
+print(f"x is {x}")
+print(f"There are {y} stand users")
+apple = input("How many apples did you eat?")
+print(f"The user said he ate {apple} apples")
+
+
