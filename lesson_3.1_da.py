bdfbb12bfdcccfe6e15f268bedb1a4d321c8dc2d
Kevin Wu <superenderman1166@gmail.com>
2022-10-17 11:45:02 -0400

    question for next class

--- lesson_3.1_da.py
+++ lesson_3.1_da.py
@@ -1,12 +1,20 @@
 from random import randint
 
 
-x = randint(1, 100)
-y = randint(1, 100)
-answer = int(input(f"{x} * {y}=?") )
-correct_answer = x * y
-if answer == correct_answer:
-    print("good job")
-else:
-    print("try again")
+# x = randint(1, 100)
+# y = randint(1, 100)
+# answer = int(input(f"{x} * {y}=?") )
+# correct_answer = x * y
+# if answer == correct_answer:
+#     print("good job")
+# else:
+#     print("try again")
 
+while True:
+    x = randint(1, 100)
+    y = randint(1, 100)
+    answer = int(input(f"{x} * {y}=?") )
+    correct_answer = x * y
+    if answer == correct_answer:
+        print("good job")
+        break
--- loop_practice.py
+++ loop_practice.py
@@ -5,13 +5,14 @@
 # for i in '101':   COMMON ERROR
 #     print("**********")
             # start, stop, step
-# for q in range(10, -11, -2):
-#     print(f"x = {q}")
+for q in range(10, -11, -2):
+    print(f"x = {q}")
 # for q in range(1, 11, 1):
-#     print(f"{q}+")
+#     print(f"{q}")
 # f = format
-result = 0
-for w in range(1, 11):
-    old_result = result
-    result = result + w
-    print(f"{old_result}+{w}={result}")
+
+#  result = 0
+# for w in range(1, 11):
+#     old_result = result
+#     result = result + w
+#     print(f"{old_result}+{w}={result}")
