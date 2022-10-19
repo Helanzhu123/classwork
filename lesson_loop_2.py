3ed023b6e4f31c0da4c0da0bbac6cdb87bf156cd
Kevin Wu <superenderman1166@gmail.com>
2022-10-18 20:56:19 -0400

    New lesson and practice

---
+++ lesson_loop_2.py
@@ -0,0 +1,15 @@
+# result = 0
+# for number in range(2, 89, 2):
+#     result = result + number
+# print(f"the answer is {result}")
+
+# result = 0
+# for number in range(2, 89, 2):
+#     result = result + number
+#     print(f"the answer is {result}")
+# *******Two different 
+
+first_number = 10000
+for answer in range(100, 0):
+    first_number = first_number - answer
+print(f"yeye final answer is {first_number}, sweet~")
