7c8a4a58b613c326d03e90e58fc4ebe31db9943d
Kevin Wu <superenderman1166@gmail.com>
2022-10-20 13:22:55 -0400

    I did it with a hint

--- guess_number.py
+++ guess_number.py
@@ -1,16 +1,19 @@
 from random import randint
 
-
+count = 0
 number = randint(1, 20)
 print("Let's play a guessing game, number between 1 to 20")
 while True:
     user_answer = int(input("The number is?"))
+    count += 1
     if number == user_answer:
         print(f"GOOD JOB MY BUDDY!The answer is {number}!")
         print("Thank You for playig this game my buddy")  
+        print(f"You tried {count} times in total.")
         break
     elif number > user_answer:
         print("TOO LOW")
     elif number < user_answer:
         print("TOO HIGH")
     # track down how many times did user tried
+# count = count + 1 is same as count += 1
