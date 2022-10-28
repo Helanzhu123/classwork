37ac778715f521a4da5ceb328dcd4df90e3f303a
Kevin Wu <superenderman1166@gmail.com>
2022-10-28 15:27:51 -0400

    new lesson

---
+++ 5_2.py
@@ -0,0 +1,7 @@
+my_list = [6,5]
+my_list.append(9)
+
+print(my_list)
+
+for n in range(5):
+    print(n)
--- practice_in_class.py
+++ practice_in_class.py
@@ -12,6 +12,20 @@ while True:
     choice = int(input("Choice: "))
     if choice == 1:
         print("Welcome to Game One")
+        print("Please clap 5 times to win the game")
+        while True: # Clapping Game
+            print(f"Claps: {clap}")
+            # menu options
+            print("[1] Clap")
+            # get choice
+            choice = int(input("Choice: "))
+            # handle choice
+            if choice == 1:
+                print("CLAP")
+                clap = 0
+            while clap < 5:
+                print(f"Claps: {clap}")
+                print("[1] Clap")
+            print("You win the game! Congrat!")
     elif choice == 2:
         print("Welcome to Game Two")
-        
