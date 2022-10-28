c816335f0718fa4e7137d4697b311aaeafaebd63
Kevin Wu <superenderman1166@gmail.com>
2022-10-28 13:53:30 -0400

    game

--- 5_1.py
+++ 5_1.py
@@ -31,20 +31,27 @@ print(my_list[0:6:2])
 
 
 print("------------------")
-
+#also very important
 print(my_list[2:])    #from 2, give everything else in the line
 print(my_list[:5])
 
 print(my_list[::-1]) #reverse a list
 
+print("-----------------")
 friends = ["Jolyne", "Sanji", "L"]
 print(friends)
 friends.append("Kitagawa")
 friends.append("Edogawa")
+for f in friends:
+    print(f, end = " ")
 print(friends)
-
+print("---------------")
 print(friends[0])
 print(friends[-5])
 print("-----------")
 pick = friends[0]
 print(pick)
+print("----------------")
+print(friends[1:5])
+print(friends[0:len(friends):3])
+print(friends[0:5:2])
---
+++ practice_in_class.py
@@ -0,0 +1,17 @@
+
+print("よこそ僕のゲムに")
+
+
+
+while True:
+    print("Please choose a mini game")
+    print()
+    print("[1] Game One")
+    print("[2] Game Two")
+
+    choice = int(input("Choice: "))
+    if choice == 1:
+        print("Welcome to Game One")
+    elif choice == 2:
+        print("Welcome to Game Two")
+        
