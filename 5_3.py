26d1d7f670435206bc53d6ca23dda2003be16270
Kevin Wu <superenderman1166@gmail.com>
2022-11-02 12:24:07 -0400

    practices

--- 5_3.py
+++ 5_3.py
@@ -7,3 +7,14 @@ for n in friends:
     if n[0].lower() == "j":
         count += 1
 print(count)
+
+i = 0
+while i < len(friends):
+    name = friends[i].lower()
+    print(f"i: {i}, name: {name}")
+    if name == "jonathan":
+        friends[i] = "Higashikata"
+        break
+    i += 1
+
+print(friends)
